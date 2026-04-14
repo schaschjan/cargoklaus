014e5a310e4e5e3a29f63ec8fcf48c8d828e04f4
schaschjan <jan-jakub@atelierdisko.de>
2026-04-14 10:57:21 +0200

    wip

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ const compat = new FlatCompat({
 })
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('next/core-nextjs-vitals', 'next/typescript'),
   {
     rules: {
       '@typescript-eslint/ban-ts-comment': 'warn',
